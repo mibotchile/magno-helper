b84b8aec3fc15ebf20c7afc2f310b699e603a1a9
drangel1629 <david@onbotgo.com>
2022-03-18 10:29:57 -0500

    some changes

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const expressValidator = require('express-validator');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const expressValidator = require("express-validator");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 // Import routes
-const index = require('./routes/index');
+const index = require("./routes/index");
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // Exposes a bunch of methods for validating data input
@@ -20,11 +18,12 @@ app.use(expressValidator());
 app.use(cookieParser());
 
 // Handle Routes
-app.use('/', index);
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  console.log("respuesta---", req);
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -33,12 +32,12 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   console.log(err);
-  res.json({ status: 400, message: 'Something Went Wrong', err });
+  res.json({ status: 400, message: "Something Went Wrong", err });
 });
 
 module.exports = app;
